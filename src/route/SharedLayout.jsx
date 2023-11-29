b95bb5bfe9dec92215fa6eb0c44a6c090186cdfe
ertyhd <orionship@protonmail.com>
2023-11-30 00:57:14 +0200

    add modal and desktop version done

--- src/route/SharedLayout.jsx
+++ src/route/SharedLayout.jsx
@@ -1,12 +1,12 @@
 import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
-
+import Modal from "../components/Modal/Modal";
 const Header = lazy(() => import("../modules/Header/Header"));
 const MainPage = lazy(() => import("../pages/MainPage/MainPage"));
 
 const SharedLayout = () => {
   return (
-    <Suspense fallback={<p>...loading</p>}>
+    <Suspense fallback={<Modal />}>
       <Header />
       <Routes>
         <Route path="/" element={<MainPage />} />
