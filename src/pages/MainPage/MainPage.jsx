9719a6086d337d15af419d7eb4ec45fc90db2d5c
ertyhd <orionship@protonmail.com>
2023-11-30 13:52:29 +0200

    add mobile & tab heder & main

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,23 +1,23 @@
 import styles from "./mainPage.module.scss";
 
 import Hero from "../../modules/Hero/Hero";
-import About from "../../modules/About/About";
-import Electricity from "../../modules/Electricity/Electricity";
-import Cases from "../../modules/Cases/Cases";
-import Faq from "../../modules/Faq/Faq";
-import ContactUs from "../../modules/ContactUs/ContactUs";
-import Footer from "../../modules/Footer/Footer";
+// import About from "../../modules/About/About";
+// import Electricity from "../../modules/Electricity/Electricity";
+// import Cases from "../../modules/Cases/Cases";
+// import Faq from "../../modules/Faq/Faq";
+// import ContactUs from "../../modules/ContactUs/ContactUs";
+// import Footer from "../../modules/Footer/Footer";
 
 const MainPage = () => {
   return (
     <section className={styles.container}>
       <Hero />
-      <About />
-      <Electricity />
-      <Cases />
-      <Faq />
-      <ContactUs />
-      <Footer />
+      {/* <About /> */}
+      {/* <Electricity /> */}
+      {/* <Cases /> */}
+      {/* <Faq /> */}
+      {/* <ContactUs /> */}
+      {/* <Footer /> */}
     </section>
   );
 };
